05a178daecdb87d7aa88b6c4a05fd32e8df9d8e7
Danny Castillo <dannyhgo@hotmail.com>
2021-01-31 01:44:38 +0100

    Add first Code

---
+++ script.js
@@ -0,0 +1,8 @@
+// Variables
+const characteristics = document.querySelectorAll('.char');
+
+// Creating Characteristics
+characteristics.forEach(char => {
+    const random = 20 + Math.floor(Math.random() * 20 + 1);
+    char.innerHTML = random;
+});
